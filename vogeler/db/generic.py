80e2757c0cc3e5763026114c0a5671f5ff7ab253
Dominic LoBue <dominic.lobue@gmail.com>
2010-09-19 00:07:09 -0700

    first try at abstracting out db interface

---
+++ vogeler/db/generic.py
@@ -0,0 +1,135 @@
+import datetime, yaml
+
+import couchdbkit as couch
+from couchdbkit.loaders import FileSystemDocsLoader
+
+from vogeler.exceptions import VogelerPersistenceException
+
+class UnknownDatatype(Exception): pass
+
+import logging
+
+class null_handler(logging.Handler):
+    def emit(self, record):
+        pass
+
+nh = null_handler()
+logger = logging.getLogger('vogeler.db')
+logger.addHandler(nh)
+
+class SystemRecord(couch.Document):
+    system_name = couch.StringProperty()
+    created_at = couch.DateTimeProperty()
+    updated_at = couch.DateTimeProperty()
+
+class generic_transport(object):
+    def connect(self, host, port):
+        l = 'Opening connection to host %s on port %i' % (host, port)
+        logger.debug(l)
+        self._connect(host, port)
+
+    def get_db(self, dbname):
+        raise NotImplemented
+
+    def drop_db(self, dbname):
+        raise NotImplemented
+
+    def get_record(self, record):
+        raise NotImplemented
+
+
+class couch_transport(object):
+    def _connect(self, host, port):
+        connection_string = "http://%s:%s" % (host, port)
+        self.server = couch.Server(uri=connection_string)
+
+    def get_db(self, dbname):
+        return self.server.get_or_create_db(dbname)
+
+    def drop_db(self, dbname):
+        return self.server.delete_db(dbname)
+
+    def get_record(self, record):
+        return self.server.get(record)
+
+class VogelerStore(object):
+
+    def __init__(self, host, port, db, transport):
+        self.dbname = db
+        self.transport = transport(host, port)
+
+    def create_db(self, dbname=None):
+        if not dbname:
+            dbname = self.dbname
+
+        self.db = self.transport.get_db(dbname)
+
+    def drop_db(self, dbname=None):
+        if not dbname:
+            dbname = self.dbname
+
+        self.transport.drop_db(dbname)
+
+    def use_db(self, dbname=None):
+        if not dbname:
+            dbname = self.dbname
+
+        self.db = self.transport.get_db(dbname)
+
+    def create(self, node_name):
+        node = SystemRecord.get_or_create(node_name)
+        node.system_name = node_name
+        node.created_at = datetime.datetime.utcnow()
+        node.save()
+
+    def get(self, node_name):
+        node = self.db.get_record(node_name)
+        return node
+
+    def touch(self, node_name):
+        node = self.get(node_name)
+        node.updated_at = datetime.datetime.utcnow()
+        node.save()
+
+    def update(self, node_name, key, value, datatype):
+        node = SystemRecord.get_or_create(node_name)
+
+        try:
+            datatype_method = getattr(self, '_update_%s' % datatype)
+            node[key] = datatype_method(node, key, value)
+        except AttributeError:
+            raise UnknownDatatype("Don't know how to handle datatype '%r'" %
+                                  datatype)
+
+        node.updated_at = datetime.datetime.utcnow()
+        node.save()
+
+    def _update_output(self, node, key, value):
+        v = [z.strip() for z in value.split("\n")]
+        return v
+
+    def _update_pylist(self, node, key, value):
+        return value
+
+    def _update_pydict(self, node, key, value):
+        return value
+
+    def _update_yaml(self, node, key, value):
+        return value
+
+    def _update_string(self, node, key, value):
+        return value
+
+    def _update_raw(self, node, key, value):
+        return value
+
+
+    def load_views(self, lp):
+        self.loadpath = lp
+        print "Loading design docs from %s" % lp
+        loader = FileSystemDocsLoader(self.loadpath)
+        loader.sync(self.db, verbose=True)
+        print "Design docs loaded"
+        return 0
+
+# vim: set ts=4 et sw=4 sts=4 sta filetype=python :
